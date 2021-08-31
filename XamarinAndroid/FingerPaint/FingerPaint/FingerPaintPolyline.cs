f1c46fd2a3edeb1cad656cd51d0f1bb86386e59a
John Marin <58445019+johmarin@users.noreply.github.com>
2021-08-31 12:56:31 -0700

    Create FingerPaintPolyline.cs

---
+++ XamarinAndroid/FingerPaint/FingerPaint/FingerPaintPolyline.cs
@@ -0,0 +1,19 @@
+  
+using Android.Graphics;
+
+namespace FingerPaint
+{
+    class FingerPaintPolyline
+    {
+        public FingerPaintPolyline()
+        {
+            Path = new Path();
+        }
+
+        public Color Color { set; get; }
+
+        public float StrokeWidth { set; get; }
+
+        public Path Path { private set; get; }
+    }
+}
